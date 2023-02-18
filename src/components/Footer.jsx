4fcfef750aaf3dd765471bde1831aff7999e6a6d
chepyego <jchepyego@gmail.com>
2023-02-18 14:25:26 +0300

    add css to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,33 +1,33 @@
 
 import {Link} from 'react-router-dom'
 import React from 'react'
+const Logo = require("../images/Group 9.png");
+
 
 function Footer() {
   return (
     <div className='primary-back'>
-        <ul className='flex pt-28'>
-            <div>
-            <li className='tracking-wide font-sans text-xl mr-96 ml-20 nav-text'>
-              <Link to='/'><span className='text-4xl font-bold'>W</span>ise <span className='font-bold text-4xl'>L</span>edger</Link>
-            </li>
+        <ul className='flex justify-center py-28'>
+            <div className='mr-16'>
+              <img  src={Logo} alt="logo"/>
         
             </div>
             <div>
                 <ul className='mr-48'>
-                    <li>Company</li>
-                    <li>Features</li>
-                    <li>About us</li>
-                    <li>Blog</li>
+                    <li className='text-4xl primary-text-color mb-8'>Company</li>
+                    <li className='text-2xl secondary-text-color font-medium mb-4'>Features</li>
+                    <li className='text-2xl mb-4 secondary-text-color font-medium'>About us</li>
+                    <li className='text-2xl secondary-text-color font-medium'>Blog</li>
 
                 </ul>
 
             </div>
             <div>
                 <ul>
-                    <li>Legal</li>
-                    <li>Terms and Conditions</li>
-                    <li>Privacy Policy</li>
-                    <li>Responsible Disclosure</li>
+                    <li className='text-4xl primary-text-color mb-8'>Legal</li>
+                    <li className='text-2xl secondary-text-color font-medium mb-4'>Terms and Conditions</li>
+                    <li className='text-2xl secondary-text-color mb-4'>Privacy Policy</li>
+                    <li className='text-2xl secondary-text-color'>Responsible Disclosure</li>
                 </ul>
 
             </div>
